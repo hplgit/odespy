7dcedff1ad9723e395ad214c721b3eece9021364
Liwei Wang <ruojiao77@yahoo.com>
2012-02-02 23:27:10 +0100

    setup.py recovered.

--- setup.py
+++ setup.py
@@ -6,10 +6,34 @@ from os.path import join
 def configuration(parent_package='',top_path=None):
 
     from numpy.distutils.misc_util import Configuration
-
+    from numpy.distutils.system_info import get_info
     config = Configuration('odesolvers', parent_package, top_path)
 
-    config.add_subpackage('odesolvers')
+    config.add_library('_odepack',
+                       sources=[join('odepack','solve_odepack.f'), 
+                                join('odepack','opkd*.f')])
+
+    config.add_library('_rkc',
+                       sources=[join('rkc','solve_rkc.f'),
+                                join('rkc','rkc.f')])
+    config.add_library('_rkf45',
+                       sources=[join('rkf45','advance_rkf45.f'),
+                                join('rkf45','rkf45.f'),
+                                join('rkf45','rkf45_associate.f')])
+
+    # Extensions:
+    # odepack
+    config.add_extension('_odepack',
+                         sources=[join('odepack','odepack.pyf')],
+                         libraries=['_odepack'])
+    config.add_extension('_rkc',
+                         sources=[join('rkc','rkc.pyf')],
+                         libraries=['_rkc'])
+    config.add_extension('_rkf45',
+                         sources=[join('rkf45','rkf45.pyf')],
+                         libraries=['_rkf45'])
+
+    config.add_data_dir('tests')
     return config
 
 if __name__ == '__main__':
