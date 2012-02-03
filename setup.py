88f5d94c31b2423c46c73edb391aa6075c9b62c0
Liwei Wang <ruojiao77@yahoo.com>
2012-02-03 13:49:29 +0100

    Update setup.py in top level.

--- setup.py
+++ setup.py
@@ -6,37 +6,19 @@ from os.path import join
 def configuration(parent_package='',top_path=None):
 
     from numpy.distutils.misc_util import Configuration
-    from numpy.distutils.system_info import get_info
-    config = Configuration('odesolvers', parent_package, top_path)
 
-    config.add_library('_odepack',
-                       sources=[join('odepack','solve_odepack.f'), 
-                                join('odepack','opkd*.f')])
+    config = Configuration(None, parent_package, top_path)
+    config.set_options(ignore_setup_xxx_py=True,
+                       assume_default_configuration=True,
+                       delegate_options_to_subpackages=True,
+                       quiet=True)
 
-    config.add_library('_rkc',
-                       sources=[join('rkc','solve_rkc.f'),
-                                join('rkc','rkc.f')])
-    config.add_library('_rkf45',
-                       sources=[join('rkf45','advance_rkf45.f'),
-                                join('rkf45','rkf45.f'),
-                                join('rkf45','rkf45_associate.f')])
+    config.add_subpackage('odesolvers')
 
-    # Extensions:
-    # odepack
-    config.add_extension('_odepack',
-                         sources=[join('odepack','odepack.pyf')],
-                         libraries=['_odepack'])
-    config.add_extension('_rkc',
-                         sources=[join('rkc','rkc.pyf')],
-                         libraries=['_rkc'])
-    config.add_extension('_rkf45',
-                         sources=[join('rkf45','rkf45.pyf')],
-                         libraries=['_rkf45'])
-
-    config.add_data_dir('tests')
     return config
 
 if __name__ == '__main__':
+
     from numpy.distutils.core import setup
     setup(**configuration(top_path='').todict())
 
